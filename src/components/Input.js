5a14d1d7351f8d801dd8bb01732f785d59ce0711
ThanhTam <20130396@st.hcmuaf.edu.vn>
2023-06-16 06:52:36 +0700

    chức năng ghi âm

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import Img from '../img/img.png';
-import Attach from '../img/attach.png';
 import { FiSend } from "react-icons/fi";
 import InputEmoji from 'react-input-emoji';
+import {FaMicrophone} from "react-icons/fa";
+import vmsg from "vmsg";
 
 function Input({ webSocketAPI, userName }) {
     const [type, setType] = useState([]);
     const [newMessage, setNewMessage] = useState("");
+    const [isLoading, setLoading]  = useState(false);
+    const [isRecording, setRecord]  = useState(false);
+    const [recordings, setRecordings]= useState([]);
+    const [recorder] = useState(new vmsg.Recorder({
+        wasmURL: "https://unpkg.com/vmsg@0.3.0/vmsg.wasm"
+    }))
+
 
     const handleChange = (newMessage) => {
         setNewMessage(newMessage);
@@ -28,23 +36,55 @@ function Input({ webSocketAPI, userName }) {
 
     const sendChat = (event) => {
         const mess = newMessage;
-        const messUtf8 = encodeURI(mess);
         event.preventDefault();
-        const data = {
-            action: "onchat",
-            data: {
-                event: "SEND_CHAT",
+        const messEncode = encodeURI(mess);
+        const mes =messEncode.replace(/%20/g," ");
+
+
+            const data = {
+                action: "onchat",
                 data: {
-                    type: type,
-                    to: userName,
-                    // mes: mess
-                    mes: messUtf8
+                    event: "SEND_CHAT",
+                    data: {
+                        type: type,
+                        to: userName,
+                        // mes: mess
+                        mes: mes
+                    }
                 }
-            }
-        };
-      webSocketAPI.send(data);
+            };
+            webSocketAPI.send(data);
+
       setNewMessage("");
     };
+
+
+
+    const record = async () => {
+        setLoading(true);
+
+        if (isRecording) {
+            const blob = await recorder.stopRecording();
+            setLoading(false);
+            setRecord(false);
+            setRecordings(recordings.concat(URL.createObjectURL(blob)));
+            console.log(recordings.toString())
+
+        } else {
+            try {
+                await recorder.initAudio();
+                await recorder.initWorker();
+                recorder.startRecording();
+               setLoading(false);
+               setRecord(true);
+            } catch (e) {
+                console.error(e);
+                setLoading(false);
+            }
+        }
+    };
+
+
 
     return (
         <form className="input" onSubmit={sendChat}>
@@ -52,7 +92,7 @@ function Input({ webSocketAPI, userName }) {
                 <div className="chat-sender">
                     <InputEmoji id="mess" value={newMessage} onChange={handleChange}   placeholder="Nhập vào tin nhắn..."/>
                 </div>
-                <img src={Attach} alt="" />
+              <button className={isRecording?"micro-btn-active" :"micro-btn" } type="button" disabled={isLoading} onClick={record}>  <FaMicrophone/> </button>
                 <input type="file" style={{ display: "none" }} id="file" />
                 <label htmlFor="file">
                     <img src={Img} alt="" />
